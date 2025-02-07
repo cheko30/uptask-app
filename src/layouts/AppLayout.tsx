96622fab4a0fb05599f755d146b808dc4a79ad81
sergio.pena <sergio.pena.orozco@gmail.com>
2025-02-07 00:45:28 -0600

    add react toastify configuration

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 import Logo from "@/components/Logo";
 import NavMenu from "@/components/NavMenu";
 
@@ -13,14 +15,21 @@ export default function AppLayout() {
                 <NavMenu />
             </div>
         </header>
+        
         <section className="ax-w-screen-2xl mx-auto mt-10 p-5">
             <Outlet/>
         </section>
+        
         <footer className="py-5">
             <p className="text-center">
                 Todos los derechos reservados {new Date().getFullYear()}
             </p>
         </footer>
+
+        <ToastContainer
+            pauseOnHover={false}
+            pauseOnFocusLoss={false}
+        />
     </>
   )
 }
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,13 @@
 import ProjectForm from "@/components/projects/ProjectForm";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
 import { ProjectFormData } from "@/types/index";
 import { createProject } from "@/api/ProjectAPI";
 
 export default function CreateProjectView() {
+
+    const navigate = useNavigate()
 
     const initialValues: ProjectFormData = {
         projectName: "",
@@ -14,8 +17,10 @@ export default function CreateProjectView() {
 
     const { register, handleSubmit, formState:{errors} } = useForm({defaultValues:initialValues})
 
-    const handleForm = (data: ProjectFormData) => {
-        createProject(data)
+    const handleForm = async (formData: ProjectFormData) => {
+        const data = await createProject(formData)
+        toast.success(data)
+        navigate('/')
     }
 
     return (
