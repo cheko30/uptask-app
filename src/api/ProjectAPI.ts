9b85a875eeafd369e1a78d8b8c16bc100b5e99e3
sergio.pena <sergio.pena.orozco@gmail.com>
2025-02-11 22:57:45 -0600

    add view for edit project

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { ProjectFormData } from "../types";
+import { dashboardProjectSchema, Project, ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData:ProjectFormData) {
@@ -11,5 +11,45 @@ export async function createProject(formData:ProjectFormData) {
             throw new Error(error.response.data.error)
         }
     }
-    
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects')
+        const response = dashboardProjectSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectById(id:Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+type ProjectAPIType = {
+    formData: ProjectFormData,
+    projectId: Project['_id']
+}
+
+export async function updateProject({formData, projectId} : ProjectAPIType) {
+    try {
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
 }
