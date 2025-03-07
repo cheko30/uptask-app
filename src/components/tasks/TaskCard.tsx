6f517526b9a148c9ac916921f7a260445621ba42
sergio.pena <sergio.pena.orozco@gmail.com>
2025-03-07 01:33:01 -0600

    add roles for projects and tasks

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -8,10 +8,11 @@ import { deleteTask } from "@/api/TaskAPI"
 import { toast } from "react-toastify"
 
 type TaskCardProps = {
-    task: Task
+    task: Task,
+    canEdit: boolean
 }
 
-export default function TaskCard({task}: TaskCardProps) {
+export default function TaskCard({task, canEdit}: TaskCardProps) {
 
     const navigate = useNavigate()
     const params = useParams()
@@ -36,6 +37,7 @@ export default function TaskCard({task}: TaskCardProps) {
                 <button
                     type="button"
                     className="text-xl font-bold text-slate-600 text-left"
+                    onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
                 >
                     {task.name}
                 </button>
@@ -60,25 +62,29 @@ export default function TaskCard({task}: TaskCardProps) {
                                     Ver Tarea
                                 </button>
                             </MenuItem>
-                            <MenuItem>
-                                <button 
-                                    type='button' 
-                                    className='block px-3 py-1 text-sm leading-6 text-gray-900'
-                                    onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
-                                >
-                                    Editar Tarea
-                                </button>
-                            </MenuItem>
+                            {canEdit && (
+                                <>
+                                    <MenuItem>
+                                        <button 
+                                            type='button' 
+                                            className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                            onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
+                                        >
+                                            Editar Tarea
+                                        </button>
+                                    </MenuItem>
 
-                            <MenuItem>
-                                <button 
-                                    type='button' 
-                                    className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                    onClick={() => mutate({projectId, taskId: task._id})}
-                                >
-                                    Eliminar Tarea
-                                </button>
-                            </MenuItem>
+                                    <MenuItem>
+                                        <button 
+                                            type='button' 
+                                            className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                            onClick={() => mutate({projectId, taskId: task._id})}
+                                        >
+                                            Eliminar Tarea
+                                        </button>
+                                    </MenuItem>
+                                </>
+                            )}
                         </MenuItems>
                     </Transition>
                 </Menu>
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,4 +1,6 @@
 import { deleteProject, getProjects } from "@/api/ProjectAPI";
+import { useAuth } from "@/hooks/useAuth";
+import { isManager } from "@/utils/polices";
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/16/solid";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -7,6 +9,8 @@ import { toast } from "react-toastify";
 import { Fragment } from "react/jsx-runtime";
 
 export default function DashboardView() {
+
+  const { data: user, isLoading: authLoading } = useAuth()
 
   const { data, isLoading } = useQuery({
     queryKey: ['projects'],
@@ -26,11 +30,9 @@ export default function DashboardView() {
     }
   })
 
-  if(isLoading) return 'Cargando...'
+  if(isLoading && authLoading) return 'Cargando...'
 
-  console.log(data)
-
-  if(data) return (
+  if(data && user) return (
     <>
       <h1 className="text-5xl font-black">Mis Proyectos</h1>
       <p className="text-2xl font-light text-gray-500 mt-5
@@ -53,6 +55,13 @@ export default function DashboardView() {
               <li key={project._id} className="flex justify-between gap-x-6 px-5 py-10">
                   <div className="flex min-w-0 gap-x-4">
                       <div className="min-w-0 flex-auto space-y-2">
+                          <div className="mb-2">
+                            {
+                              isManager(project.manager, user._id)
+                                ? <p className="font-bold text-xs uppercase bg-indigo-50 text-indigo-500 border-2 border-indigo-500 rounded-lg inline-block py-1 px-5">Manager</p>
+                                : <p className="font-bold text-xs uppercase bg-green-50 text-green-500 border-2 border-green-500 rounded-lg inline-block py-1 px-5">Colaborador</p>
+                            }
+                          </div>
                           <Link to={`/projects/${project._id}`}
                               className="text-gray-600 cursor-pointer hover:underline text-3xl font-bold"
                           >{project.projectName}</Link>
@@ -83,21 +92,27 @@ export default function DashboardView() {
                                           Ver Proyecto
                                           </Link>
                                       </MenuItem>
-                                      <MenuItem>
+                                      {isManager(project.manager, user._id) && (
+                                        <>
+                                          <MenuItem>
                                           <Link to={`/projects/${project._id}/edit`}
                                               className='block px-3 py-1 text-sm leading-6 text-gray-900'>
-                                          Editar Proyecto
-                                          </Link>
-                                      </MenuItem>
-                                      <MenuItem>
-                                          <button 
-                                              type='button' 
-                                              className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                              onClick={() => mutate(project._id)}
-                                          >
-                                              Eliminar Proyecto
-                                          </button>
-                                      </MenuItem>
+                                            Editar Proyecto
+                                            </Link>
+                                        </MenuItem>
+                                        <MenuItem>
+                                            <button 
+                                                type='button' 
+                                                className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                                onClick={() => mutate(project._id)}
+                                            >
+                                                Eliminar Proyecto
+                                            </button>
+                                        </MenuItem>
+                                        </>
+                                      )
+
+                                      }
                               </MenuItems>
                           </Transition>
                       </Menu>
